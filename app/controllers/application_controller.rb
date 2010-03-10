7b8724fca80b9fbace14cb0e143273532fbbff78
naut <neonaut>
2010-03-10 15:33:42 +0100

    registraion fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,54 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
+
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  layout "application"
+  protect_from_forgery
 
-  helper_method :current_user
+  helper :all
+  helper_method :logged_in?, :current_user_session, :current_user
+  filter_parameter_logging :password, :password_confirmation
 
-  private
+protected    
 
-  def current_user_session
-    return @current_user_session if defined?(@current_user_session)
-    @current_user_session = UserSession.find
+  def store_location
+    session[:return_to] = request.request_uri
   end
+
+  def redirect_back_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
+  end 
 
   def current_user
-    return @current_user if defined?(@current_user)
-    @current_user = current_user_session && current_user_session.record
+    @current_user ||= current_user_session and current_user_session.user
+  end 
+
+  def current_user_session
+    @current_user_session ||= UserSession.find
+  end 
+
+  def logged_in?
+    not current_user.nil?
+  end 
+
+  def require_user
+    unless logged_in?
+      store_location
+      flash[:notice] = 'Please login in to access this page!'
+      redirect_to new_user_session_url
+      return false
+    end
   end
 
+  def require_no_user
+    if logged_in?
+      store_location
+      flash[:notice] = 'Please logout to access this page!'
+      redirect_to account_url
+      return false
+    end
+  end
 
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
 end
