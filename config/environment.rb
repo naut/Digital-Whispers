0267d494b9045bcde11b44f7f7ab757bb5eb366c
naut <neonaut>
2010-03-10 16:38:44 +0100

    reg fix

--- config/environment.rb
+++ config/environment.rb
@@ -6,8 +6,6 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
-
-
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
@@ -43,7 +41,9 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
   
-  
-  
+end
+
+class ActionMailer::Base
+  default_url_options[:host] = 'digital-whispers.net'
 end
 
