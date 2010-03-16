56db3f86c0b3418f1a99040dcccdb8011183722d
naut <neonaut>
2010-03-16 21:36:48 +0100

    new feeds

--- config/environment.rb
+++ config/environment.rb
@@ -14,6 +14,8 @@ Rails::Initializer.run do |config|
   
   ENV['RAILS_ASSET_ID'] = ""
   
+  config.action_controller.allow_forgery_protection = false
+  
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
