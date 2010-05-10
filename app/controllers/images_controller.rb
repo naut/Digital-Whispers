bcb15a699f8745965aa5ff00dd3fa6d3db43e14f
naut <neonaut>
2010-05-10 16:18:17 +0200

    fix

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -58,7 +58,8 @@ class ImagesController < ApplicationController
      require 'tempfile'
 
      logger.info {params[:image]}
-     img = Magick::Image.read_inline(params[:image]).first
+     img = Magick::Image.from_blob(params[:image])
+     
      
      
      #img.write("#{RAILS_ROOT}/public/test.jpg")
