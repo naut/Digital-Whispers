8edb1db6c8d50c3c658983bb4b4fd649b34bae3b
naut <neonaut>
2010-05-26 01:59:54 +0200

    fixes

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -6,6 +6,18 @@ class ImagesController < ApplicationController
 
   def index
     @images = Image.all
+    if logged_in?
+      @user_entries = Entry.find(:all, :conditions => ['owner_id=?',current_user.id])
+      @user_submissions = Array.new
+      for entry in @user_entries
+        for image in entry.images
+          @user_submissions << image.id.to_s+","
+        end
+      end
+    end
+    
+  
+    
   end
   
   def show
